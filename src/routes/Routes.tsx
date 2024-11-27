52e0188d23ad5ca1c211cf88099b52932dfba429
margarita <146252026+mbrita@users.noreply.github.com>
2024-11-28 00:16:32 +0300

    add routes

---
+++ src/routes/Routes.tsx
@@ -0,0 +1,16 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import App from "../App"
+
+import { HomePage } from "../pages/homePage/HomePage"
+
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  )
+}
